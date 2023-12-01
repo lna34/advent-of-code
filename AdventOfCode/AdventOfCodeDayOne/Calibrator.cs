82675d0ec6218a82e80963750f0ecd94a281dbfc
Luc <luc.nabhan@gmail.com>
2023-12-01 21:10:34 +0100

    Ajoutez des fichiers projet.

---
+++ AdventOfCode/AdventOfCodeDayOne/Calibrator.cs
@@ -0,0 +1,21 @@
+﻿namespace AdventOfCode.AdventOfCodeDayOne
+{
+    public class Calibrator
+    {
+        private string[] _linesToBeDecoded;
+
+        public Calibrator(string[] linesToBeDecoded) 
+        {
+            _linesToBeDecoded = linesToBeDecoded; 
+        }
+
+        public int Resolve()
+        {
+            return _linesToBeDecoded
+                .Select(line => new string(line.Where(c => char.IsDigit(c)).ToArray()))
+                .Where(line => !string.IsNullOrEmpty(line))
+                .Select(line => Convert.ToInt32($"{line.First()}{line.Last()}"))
+                .Sum();
+        }
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,5 @@
+﻿using AdventOfCode.AdventOfCodeDayOne;
+
+string[] linesToBeDecoded = File.ReadAllLines(@"C:\Users\Luc\source\repos\AdventOfCode\AdventOfCode\AdventOfCodeDayOne\data.txt");
+int dayOneResult = new Calibrator(linesToBeDecoded).Resolve();
+Console.WriteLine(dayOneResult);
