139514674798a8592806b1336cbf66f9a3ccbdeb
Luc <luc.nabhan@gmail.com>
2023-12-03 22:59:25 +0100

    day 2 - refacto

--- AdventOfCode/Day2/Resolver.cs
+++ AdventOfCode/Day2/Resolver.cs
@@ -3,7 +3,7 @@
     public class Resolver : BaseResolver
     {
         private readonly Game _gameBag;
-        private readonly IReadOnlyCollection<Game> _games;
+        private readonly IEnumerable<Game> _games;
         public Resolver() : base(2)
         {
             _gameBag = new Game(0, new Set[]
@@ -14,39 +14,25 @@
             _games = ParseGameData();
         }
 
-        public override object ResolvePuzzle1()
-        {
-            return _games.Select(game => game.GetSum(_gameBag)).Sum();
-        }
+        public override object ResolvePuzzle1() => _games.Select(game => game.GetSum(_gameBag)).Sum();
+        public override object ResolvePuzzle2() => _games.Select(game => game.GetPowerSet()).Sum();
 
-        public override object ResolvePuzzle2()
+        private IEnumerable<Game> ParseGameData()
         {
-            return _games.Select(game => game.GetPowerSet()).Sum();
-        }
-
-        private List<Game> ParseGameData()
-        {
-            char[] setDelimiters = { ':', ';' };
-            char cubesDelimiter = ',';
-
             return Data.Select((game, index) =>
             {
-                var sets = game.Split(setDelimiters, StringSplitOptions.None).Select(set =>
-                {
-                    var cubes = set.Split(cubesDelimiter, StringSplitOptions.None);
-                    var red = GetCubeCount(cubes, nameof(Color.Red).ToLower());
-                    var green = GetCubeCount(cubes, nameof(Color.Green).ToLower());
-                    var blue = GetCubeCount(cubes, nameof(Color.Blue).ToLower());
-                    return new Set(red, green, blue);
-                }).ToArray();
+                var sets = game.Split(new char[] { ':', ';' }, StringSplitOptions.None).Select(set =>
+                 {
+                     var cubes = set.Split(',', StringSplitOptions.None);
+                     return new Set(GetCubeValue(cubes, Color.Red), GetCubeValue(cubes, Color.Green), GetCubeValue(cubes, Color.Blue));
+                 }).ToArray();
 
                 return new Game(index, sets);
-            }).ToList();
+            });
         }
 
-        private int GetCubeCount(string[] cubes, string color)
-        {
-            return cubes.Where(_ => _.Contains(color)).Sum(_ => int.Parse(_.Split(" ", StringSplitOptions.None)[1])); 
-        }
+        private int GetCubeValue(string[] cubes, Color color) => cubes.Where(
+                _ => _.Contains(color.ToString().ToLower())
+            ).Sum(_ => int.Parse(_.Split(" ", StringSplitOptions.None)[1]));
     }
 }
