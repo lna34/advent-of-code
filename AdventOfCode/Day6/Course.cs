796f167676c9e1f6534294ba2a61cfd1d13e0e92
Luc <luc.nabhan@gmail.com>
2023-12-07 18:18:49 +0100

    day 6

---
+++ AdventOfCode/Day6/Course.cs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode.Day6
+{
+    public class Course
+    {
+        public long BestDistance { get; }
+        public long MaximumAllowedTime { get; }
+
+        public Course(long maximumAllowedTime, long bestDistance)
+        {
+            BestDistance = bestDistance;
+            MaximumAllowedTime = maximumAllowedTime;
+        }
+
+        public long ComputeNumberOfSuccess()
+        {
+            long recordBeatenCounter = 0;
+            for (int buttonPressed = 0; buttonPressed < MaximumAllowedTime; buttonPressed++)
+            {
+                if (buttonPressed * (MaximumAllowedTime - buttonPressed) > BestDistance)
+                {
+                    recordBeatenCounter++;
+                }
+            }
+            return recordBeatenCounter;
+        }
+    }
+}
---
+++ AdventOfCode/Day6/Resolver.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode.Day6
+{
+    public class Resolver : BaseResolver
+    {
+        private readonly List<Course> _courses;
+        public Resolver(): base(6)
+        {
+            _courses = ParseCourseData();
+        }
+
+        public override object ResolvePuzzle1() => _courses.Select(_ => _.ComputeNumberOfSuccess()).Aggregate((curr, next) => curr*next);
+
+        public override object ResolvePuzzle2()
+        {
+            var time = "";
+            var distance = "";
+
+            foreach (var course in _courses)
+            {
+                time += course.MaximumAllowedTime.ToString();
+                distance += course.BestDistance.ToString();
+            }
+
+            var result = new Course(long.Parse(time), long.Parse(distance)).ComputeNumberOfSuccess();
+
+            return result;
+        }
+
+        public List<Course> ParseCourseData()
+        {
+            var time = Data[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            var distance = Data[1].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            return time.Select((temps, index) => new Course(long.Parse(time[index]), long.Parse(distance[index]))).ToList();
+        }
+    }
+}
